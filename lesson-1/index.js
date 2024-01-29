61cc260d8c6c18dc801298bd3931fe89722370f0
mukhriddin-dev <khodievm@gmail.com>
2024-01-29 09:34:37 +0500

    DOM elements , selectors , styling

--- lesson-1/index.js
+++ lesson-1/index.js
@@ -1,26 +1,3 @@
 "use strict";
 
-/// ----------- Get element /s  by (id , tagName , className)
 
-// let h1 = document.getElementById("text");
-// let p = document.getElementsByTagName("p");
-// let listItem = document.getElementsByClassName("item");
-// let h11 = document.getElementsByTagName("h1");
-// let list = document.getElementsByName("list");
-
-// ------------ QUERY SELECTORS --------------------
-
-// let message = prompt("send message:");
-
-// let h1 = document.querySelector("#text");
-// let h11 = document.querySelector(".text");
-// let h1tag = document.querySelector("h1");
-
-// // h11.innerHTML = `Saytga xush kelibsiz: ${message}`;
-
-// console.log(h1.innerHTML);
-// console.log(h1.textContent);
-// console.log(h1.outerHTML);
-let myCard = document.querySelector(".card");
-
-myCard.innerHTML = str;
---
+++ lesson-2/index.js
@@ -0,0 +1,96 @@
+"use strict";
+
+/*
+
+document.getElementById() -> element 
+document.getElementsByTagName() -> HTMLCollection Element []
+document.getElementsByClassName() -> HTMLCollection Element []
+document.getElementsByName() -> HTMLCollection Element []
+
+
+document.querySelector('select_type') -> element
+document.querySelectorAll('select_type') -> NodeList []
+
+element.parentElement -> return element of parent element
+
+*/
+
+// let parent = document.querySelector('.parent'),
+//     child = document.querySelector('.child'),
+//     firstTitle = document.querySelector('.child h1'),
+//     secondTitle = document.querySelector('.child h2'),
+//     paragraph = document.querySelector('.child p');
+// let otherTitle = document.querySelectorAll('h1');
+
+
+// console.log(child.parentElement.parentElement.parentElement);
+
+// console.log(parent.parentNode.parentNode.parentNode);
+
+// console.log(parent.nextElementSibling); 
+
+// console.log(parent.previousElementSibling);
+
+// console.log(parent.childNodes);
+
+// console.log(parent.firstChild);
+// console.log(parent.lastChild);
+
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+
+// console.log(parent);
+
+// console.log(parent.style); // CSS styles declaration
+
+
+// parent.style.backgroundColor="limegreen";
+// child.style.backgroundColor="coral";
+// child.style.borderRadius="20px"
+// child.style.color="white";
+
+
+// let isDark = false;
+
+// function darkMode() {
+
+//     isDark = !isDark;
+
+//     if (isDark) {
+//         parent.style.backgroundColor = "black";
+//         parent.style.color = "white";
+        
+//     } else {
+
+//         parent.style.backgroundColor = "white";
+//         parent.style.color = "black";
+
+//     }
+
+//     console.log("checking");
+
+// }
+
+// console.log(child.style);
+
+// let obj={
+//     key:""
+// }
+// console.log(obj);
+// obj.key=1;
+// console.log(obj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
