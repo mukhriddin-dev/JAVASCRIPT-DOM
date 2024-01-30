0336761138432268ba1aaec6b0e1a8fbcfb100e7
mukhriddin-dev <khodievm@gmail.com>
2024-01-30 07:57:47 +0500

    fix

--- lesson-2/index.js
+++ lesson-2/index.js
@@ -1,86 +1,143 @@
-"use strict";
+// "use strict";
 
-/*
+// /*
 
-document.getElementById() -> element 
-document.getElementsByTagName() -> HTMLCollection Element []
-document.getElementsByClassName() -> HTMLCollection Element []
-document.getElementsByName() -> HTMLCollection Element []
-
-
-document.querySelector('select_type') -> element
-document.querySelectorAll('select_type') -> NodeList []
-
-element.parentElement -> return element of parent element
-
-*/
-
-// let parent = document.querySelector('.parent'),
-//     child = document.querySelector('.child'),
-//     firstTitle = document.querySelector('.child h1'),
-//     secondTitle = document.querySelector('.child h2'),
-//     paragraph = document.querySelector('.child p');
-// let otherTitle = document.querySelectorAll('h1');
+// document.getElementById() -> element 
+// document.getElementsByTagName() -> HTMLCollection Element []
+// document.getElementsByClassName() -> HTMLCollection Element []
+// document.getElementsByName() -> HTMLCollection Element []
 
 
-// console.log(child.parentElement.parentElement.parentElement);
+// document.querySelector('select_type') -> element
+// document.querySelectorAll('select_type') -> NodeList []
 
-// console.log(parent.parentNode.parentNode.parentNode);
+// element.parentElement -> return element of parent element
 
-// console.log(parent.nextElementSibling); 
+// */
 
-// console.log(parent.previousElementSibling);
-
-// console.log(parent.childNodes);
-
-// console.log(parent.firstChild);
-// console.log(parent.lastChild);
-
-// console.log(parent.firstElementChild);
-// console.log(parent.lastElementChild);
-
-// console.log(parent);
-
-// console.log(parent.style); // CSS styles declaration
+// // let parent = document.querySelector('.parent'),
+// //     child = document.querySelector('.child'),
+// //     firstTitle = document.querySelector('.child h1'),
+// //     secondTitle = document.querySelector('.child h2'),
+// //     paragraph = document.querySelector('.child p');
+// // let otherTitle = document.querySelectorAll('h1');
 
 
-// parent.style.backgroundColor="limegreen";
-// child.style.backgroundColor="coral";
-// child.style.borderRadius="20px"
-// child.style.color="white";
+// // console.log(child.parentElement.parentElement.parentElement);
+
+// // console.log(parent.parentNode.parentNode.parentNode);
+
+// // console.log(parent.nextElementSibling); 
+
+// // console.log(parent.previousElementSibling);
+
+// // console.log(parent.childNodes);
+
+// // console.log(parent.firstChild);
+// // console.log(parent.lastChild);
+
+// // console.log(parent.firstElementChild);
+// // console.log(parent.lastElementChild);
+
+// // console.log(parent);
+
+// // console.log(parent.style); // CSS styles declaration
 
 
-// let isDark = false;
+// // parent.style.backgroundColor="limegreen";
+// // child.style.backgroundColor="coral";
+// // child.style.borderRadius="20px"
+// // child.style.color="white";
 
-// function darkMode() {
 
-//     isDark = !isDark;
+// // let isDark = false;
 
-//     if (isDark) {
-//         parent.style.backgroundColor = "black";
-//         parent.style.color = "white";
+// // function darkMode() {
+
+// //     isDark = !isDark;
+
+// //     if (isDark) {
+// //         parent.style.backgroundColor = "black";
+// //         parent.style.color = "white";
         
-//     } else {
+// //     } else {
 
-//         parent.style.backgroundColor = "white";
-//         parent.style.color = "black";
+// //         parent.style.backgroundColor = "white";
+// //         parent.style.color = "black";
 
-//     }
+// //     }
 
-//     console.log("checking");
+// //     console.log("checking");
 
-// }
+// // }
 
-// console.log(child.style);
+// // console.log(child.style);
+
+// // let obj={
+// //     key:""
+// // }
+// // console.log(obj);
+// // obj.key=1;
+// // console.log(obj);
+
+
 
 // let obj={
-//     key:""
+//     width:0
 // }
-// console.log(obj);
-// obj.key=1;
-// console.log(obj);
+
+// obj.width=200;
 
 
+
+
+// let card=document.querySelector(".card");
+
+// card.style.width="400px";
+// card.style.height="400px";
+// card.style.backgroundColor="aqua";
+// card.style.borderRadius="20px";
+// card.style.border="2px solid red";
+// card.style.margin="220px auto";
+// card.style.display="flex";
+// card.style.justifyContent="center";
+// card.style.alignItems="center";
+
+
+// card.style.cssText=`
+// color:green;
+// font-size:40px;
+// width:400px;
+// height:400px;
+// margin:140px auto;
+// background-color:red;
+// display:flex; justify-content:center;
+// align-items:center`;
+
+// console.log(card.style);
+// card.innerHTML="<h1>Dom</h1>";
+// card.children[0].style.textTransform="uppercase"
+// let list=document.querySelectorAll(".list"),
+//     li=document.querySelectorAll(".item");
+
+
+// const cards1=document.getElementsByClassName('card');
+// const cards2=document.querySelectorAll('.card');
+
+// console.log(cards1);
+// console.log(cards2);
+
+
+let box=document.querySelector(".box");
+
+
+console.log(box.clientWidth);
+console.log(box.clientHeight);
+console.log(box.offsetWidth);
+console.log(box.offsetHeight);
+console.log(box.scrollHeight);
+console.log(box.offsetTop)
+console.log(box.offsetRight);
 
 
 
---
+++ lesson-2/readem.md
@@ -0,0 +1,3 @@
+## https://tr.javascript.info/size-and-scroll
+
+![Alt text](image.png)
